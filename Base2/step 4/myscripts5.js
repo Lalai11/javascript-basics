53c3fc1e7be1b55c19458e144bd963cc519880d8
Laize Ferraz <laize@renan.home>
2019-07-10 23:27:23 +1000

    Base 2 Step4 not changing values in HTML file

--- Base2/step 4/myscripts5.js
+++ Base2/step 4/myscripts5.js
@@ -1,10 +1,17 @@
 function clearAll(){
+  
   let answer = confirm("Do you want to reset form?");
+  
   if (answer) {
+   
     document.getElementById("myForm").reset();
+    
+    let inputs = document.querySelectorAll("input[type=text]");
+    
+    for (i = 0; i < inputs.length; i++){
+      inputs[i].value = "";
+    }  
 
-  }else{
-    alert("not cleared");
   }
 
 }
